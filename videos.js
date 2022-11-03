f81440a50218a1ca74e41e30a16d94f650cb54dd
pkill-preston <0v3rthinker@protonmail.com>
2022-11-03 12:05:20 -0300

    adding new files for downloads

---
+++ videos.js
@@ -0,0 +1,8 @@
+const youtubeMp3Converter = require("youtube-mp3-converter");
+
+exports.videoConvert = async function videoConvert(videoId, title){
+  const convertLinkToMp3 = youtubeMp3Converter('./songs/')
+  await convertLinkToMp3(videoId, {
+    title: title
+  })
+}

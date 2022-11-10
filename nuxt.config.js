ade702f4f537607ee9b8f7d4b9bbbc63313d9c10
pkill-preston <0v3rthinker@protonmail.com>
2022-11-10 18:31:48 -0300

    fix: preparing to deplay on vercel

--- nuxt.config.js
+++ nuxt.config.js
@@ -71,10 +71,10 @@ export default {
   /*
   ** For deployment you might want to edit host and port
   */
-  // server: {
-  //  port: 8000, // default: 3000
-  //  host: '0.0.0.0' // default: localhost
-  // },
+  server: {
+   port: 3000, // default: 3000
+   host: '0.0.0.0' // default: localhost
+  },
 
   /*
   ** Build configuration
